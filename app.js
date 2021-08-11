595cf7b12fe03d11f3f37c901379812f7a58f20a
noe <noe.reyes92@yahoo.com>
2021-08-11 08:50:54 -0500

    created a new file for projectile class

--- app.js
+++ app.js
@@ -3,28 +3,6 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = innerWidth; //innerwidth is a property of the WINDOW object
 canvas.height = innerHeight;
-
-// Objects
-
-
-class Projectile {
-    constructor(x, y, radius, color, velocity, gravity) {
-        this.x = x
-        this.y = y
-        this.radius = radius
-        this.color = color
-        this.velocity = velocity
-        this.gravity = gravity
-    }
-
-    draw() { // I need to refactor this method since its being used in this class and the Player class
-        ctx.beginPath()
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-        ctx.fillStyle = this.color
-        ctx.fill()
-    }
-}
-
 const y = canvas.height / 2;
 
 /* addEventListener("keypress", function (e) { // No need to reference the WINDOW OBJECT, JavaScript does it IMPLICITLY
---
+++ projectile.js
@@ -0,0 +1,17 @@
+class Projectile {
+    constructor(x, y, radius, color, velocity, gravity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+        this.gravity = gravity
+    }
+
+    draw() { // I need to refactor this method since its being used in this class and the Player class
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+}
