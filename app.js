3dc6f2b08a09b640f3458f30b7c5073118c57d55
noe <noe.reyes92@yahoo.com>
2021-08-26 20:57:43 -0500

    adding form via javascript

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-// reference to the CANVAS element
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector('canvas'); // reference to the CANVAS element
 const ctx = canvas.getContext('2d'); // context - i need to figure out why this is needed?
-
 canvas.width = innerWidth; //innerwidth is a property of the WINDOW object
 canvas.height = innerHeight - 2; // resized canvas height so it fits browser 
-
 const scoreElement = document.querySelector('#scoreElement');
 console.log(scoreElement);
-
 let animationId; // confirm what this variable does again
 const enemies = [];
-
 let seconds = 00;
 let tens = 00;
 let appendTens = document.querySelector('#tens');
@@ -18,7 +13,6 @@ let appendSeconds = document.querySelector('#seconds');
 let interval;
 
 // function will run when GAME begins 
-
 function startTimer() {
     tens++;
     if (tens < 9) {
@@ -37,6 +31,21 @@ function startTimer() {
 
 interval = setInterval(startTimer, seconds);
 
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM Content Loaded");
+
+})
+
+login_bttn.addEventListener("click", () => {
+    console.log("login_bttn clicked");
+    canvas.style.display="none";
+    
+    let pElem = document.createElement('p');
+    pElem.textContent = 'This is a newly-added paragraph.';
+    document.body.appendChild(pElem);
+})
+
+
 function stopTimer() {
     clearInterval(interval);
     interval = 0;
@@ -46,3 +55,5 @@ function stopTimer() {
 // freeze frame should display player object - need to fix bug where player is being frozen with multiple frames
 // score will be determined by amount of time before collision of canvas and enemy objects occurs
 // 
+
+
