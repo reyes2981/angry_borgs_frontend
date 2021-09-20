f527e8f3b2ba30c51e9fb262a11cc937ed0a979c
noe <noe.reyes92@yahoo.com>
2021-09-19 23:08:17 -0500

    working on load logs button

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const form = document.createElement('form');
 const title = document.createElement("input");
 const textBoxBody = document.createElement("input");
 const submitBttn = document.createElement("input");
+const main = document.createElement("main");
 const logList = []; //list of veteran iniquiries
 const endPoint = "http://localhost:3000/api/v1/logs";
 
@@ -17,8 +18,6 @@ submitBttn.type = "submit";
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM Content Loaded")
-    log.getLogs();
-
 })
 
 renderLog.addEventListener('click', (e) => {
@@ -30,47 +29,11 @@ renderLog.addEventListener('click', (e) => {
 
 displayLogs.addEventListener('click', (e) => {
     e.preventDefault();
-    log.renderLogs();
-   
-
+    log.loadLogs();
 }, { once: true })
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    log.postLogs();
 })
-
-
-
-//GET veteran job iniquiries
-/* function getVJS() {
-    fetch('http://localhost:3000/api/v1/logs')
-        .then(response => response.json())
-        .then(jsonResponse => {
-            console.log(jsonResponse);
-        });
-} */
-//const jobPostingEle = document.createElement('div');
-
-// SIGN UP - POST REQUEST - CREATE
-/* function createLog() { // Going to hit CREATE method in backend API
-    const formData = {
-        title: title.value,
-        body: textBoxBody.value
-    }
-
-    const configObject = {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-        },
-        body: JSON.stringify(formData)
-    }
-
-    fetch('http://localhost:3000/api/v1/logs', configObject)
-        //Then with the data from the response in JSON...
-        .then((log) => {
-            console.log('Success:', log);
-        })
-} */
 
--- log.js
+++ log.js
@@ -1,37 +1,52 @@
 class Log {
-
-    async getLogs() {
-        const response = await fetch(endPoint);
-        let data = await response.json();
-        console.log(data);
+    constructor(title, textBoxBody) {
+        this.title = title;
+        this.textBoxBody = textBoxBody;
     }
 
-    async renderLogs() {
-        const logsContainer = document.getElementById("display-logs-bttn");
-        console.log(logsContainer);
-
+    async loadLogs() {
+        const response = await fetch(endPoint);
+        let data = await response.json()
+        logList.push(data);
+        logList.map(log => {
+            console.log(log);
+        })
     }
 
     async postLogs() {
-        const configObj = {
-            method: 'POST',
+        const userData = {
+            title: title.value,
+            body: textBoxBody.value
+        }
+        const responseData = await fetch(endPoint, {
+            method: 'POST', // Change the request method
             headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/json' // Change the Content-Type
             },
-            body: JSON.stringify(data)
-        }
-        const response = await fetch(endPoint, configObj)
-        //const json = await response.json()
-        if (response.ok) {
-            //return json
-            return response
-        } else {
-            
-        }
-    } catch(error) {
-
+            body: JSON.stringify(userData) // Add data you want to send
+        })
+        const responseJSON = await responseData.json() // Parse the response as a JSON
+        console.log(responseJSON) // Log the JSON
     }
+
+    async logCard(log) {
+        const div = document.createElement('div');
+        const h1 = document.createElement('h1');
+        const ul = document.createElement('ul');
+
+        div.setAttribute("class", "log-container");
+        div.setAttribute("data-id", log.id);
+        h1.innerHTML = log.title;
+        div.append(ul);
+        ul.append(h1);
+        document.body.appendChild(div);
+        logList.map( log => log.renderLog(log))
+    }
+
+    async renderLog(log) {
+        console.log("logs rendered");
+    }
+
 }
 
 const log = new Log();
