b4d7a79e568542b314edd2c380007e98ec9b2a13
noe <noe.reyes92@yahoo.com>
2021-09-24 18:48:42 -0500

    created universal class method file

--- app.js
+++ app.js
@@ -1,43 +1,24 @@
-//Global variables
-const createLog = document.getElementById("vet-seeking-job-bttn"); //veteran seeking job button
-const jobPostContainer = document.getElementById("job-postings-container");
-const displayLogs = document.getElementById("display-logs-bttn");
-const form = document.createElement('form');
-const title = document.createElement("input");
-const textBoxBody = document.createElement("input");
-const submitBttn = document.createElement("input");
-const main = document.createElement("main");
-let logList = []; //list of veteran iniquiries
-const endPoint = "http://localhost:3000/api/v1/logs";
+// reference to the CANVAS element
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d'); // context - i need to figure out why this is needed?
 
-form.classList = "flex flex-col justify-center items-center";
-title.setAttribute("placeholder", "title");
-textBoxBody.setAttribute("placeholder", "write here...");
-textBoxBody.setAttribute("maxLength", "244");
-submitBttn.type = "submit";
+canvas.width = innerWidth; //innerwidth is a property of the WINDOW object
+canvas.height = innerHeight - 2; // resized canvas height so it fits browser 
 
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOMContentLoaded");
-    log.loadLogs().then( (log) => {
-        
-    })
-});
+const scoreElement = document.querySelector('#scoreElement');
+console.log(scoreElement);
 
-createLog.addEventListener('click', (e) => {
-    e.preventDefault();
-    form.append(title, textBoxBody, submitBttn);
-    document.body.appendChild(form);
-    console.log('render log form');
-}, { once: true })
+let animationId; // confirm what this variable does again
+let score = 0;
+const enemies = [];
 
-displayLogs.addEventListener('click', (e) => {
-    e.preventDefault();
-    log.logCard();
-}, { once: true })
+let seconds = 00;
+let tens = 00;
+let appendTens;
+let interval;
 
-//POST fetch - creates a new job inquiry
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    log.postLogs();
-})
+//TODO 
+// freeze frame should display player object - need to fix bug where player is being frozen with multiple frames
+// score will be determined by amount of time before collision of canvas and enemy objects occurs
+// 
 
---
+++ universal_methods.js
@@ -0,0 +1,27 @@
+class UniClaMethods { 
+    draw() { 
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+
+    update() {
+        this.velocity += this.gravity;
+        this.velocity += 0;
+        this.y += this.velocity;
+        this.draw();
+
+        if (this.y > canvas.height) { // why did I need to add canvas before height?
+            this.y = canvas.height;
+            this.velocity = 0;
+        } 
+
+        if (this.y < 0) { // why did I need to add canvas before height?
+            this.y = 0;
+            this.velocity = 0;
+        } 
+
+       
+    }
+}
