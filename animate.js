12bc24b73f8142d7d458799f0cb27771a1ea456d
noe <noe.reyes92@yahoo.com>
2021-09-24 18:46:24 -0500

    renamed log file back to animate

---
+++ animate.js
@@ -0,0 +1,27 @@
+function animate() { 
+    animationId = requestAnimationFrame(animate); // what does this function do? By default  returns the value of the frame you are currently on
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height) // what exactly does this do?
+    player.draw();
+    player.update();
+
+    enemies.forEach(enemy => { // I need to change this to .MAP
+        enemy.update();
+        const distance = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+        //console.log(distance);
+        
+        // ENDGAME
+        if (distance - player.radius - enemy.radius < 1) { // if the distance between player and enemy radius is less than 1 the game is paused
+         cancelAnimationFrame(animationId); // do more research on this function
+            // inscrease our score - I'm using TIME
+            
+       /*      score += 100;
+            scoreElement.innerHTML = score; */
+        }
+    })
+
+    //TODO
+
+}
+
+animate();
